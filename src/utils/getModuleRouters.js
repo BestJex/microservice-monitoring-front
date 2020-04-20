9dbfcbb51484797c5ca8ca78a8f06f2ec75fbf02
chenmiaoyang <miaoyang.chen@hand-china.com>
2020-04-20 23:09:22 +0800

    [MOD]增加调度服务、消息服务

--- src/utils/getModuleRouters.js
+++ src/utils/getModuleRouters.js
@@ -1,12 +1,20 @@
 import { getModuleRouters } from 'utils/utils';
 import * as hzeroFrontHpfmRouters from 'hzero-front-hpfm/lib/utils/router';
 import * as hzeroFrontHiamRouters from 'hzero-front-hiam/lib/utils/router';
+import * as hzeroFrontHcnfRouters from 'hzero-front-hcnf/lib/utils/router';
+import * as hzeroFrontHmsgRouters from 'hzero-front-hmsg/lib/utils/router';
+import * as hzeroFrontHcuzRouters from 'hzero-front-hcuz/lib/utils/router';
+import * as hzeroFrontHsdrRouters from 'hzero-front-hsdr/lib/utils/router';
 import * as microServiceMonitoringLogsRouters from '../../packages/microservice-monitoring-front-logs/lib/utils/router';
 import * as microServiceMonitoringWarningRouters from '../../packages/microservice-monitoring-front-warning/lib/utils/router';
 
 export default app => getModuleRouters(app, [
   hzeroFrontHpfmRouters,
   hzeroFrontHiamRouters,
+  hzeroFrontHcnfRouters,
+  hzeroFrontHmsgRouters,
+  hzeroFrontHcuzRouters,
+  hzeroFrontHsdrRouters,
   microServiceMonitoringLogsRouters,
   microServiceMonitoringWarningRouters,
 ]);
